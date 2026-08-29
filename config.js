cb7ea829a54752936dc9698fb1d4eaa51d4f84d4
AliceP-R <alice.pottier@free.fr>
2026-08-29 16:01:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@
  *
  * Mets à jour l'URL d'API ici et elle s'applique automatiquement à tous les fichiers
  */
- 
+
 const API_CONFIG = {
     // URL du Google Apps Script déployé
     apiUrl: 'https://script.google.com/macros/s/AKfycbxTifn-zoUbBWEMw6pEIc3xlHBFJS1QBUGGOAOW0SDzhf4cu2k57PiycaXvuaWKmQH61g/exec',
- 
+
     // Autres paramètres utiles
     refreshInterval: 5 * 60 * 1000, // 5 minutes
     timezone: 'Europe/Paris'
